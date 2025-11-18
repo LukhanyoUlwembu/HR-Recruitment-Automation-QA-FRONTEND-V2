69a5734c62857583645097dac2a2b520b2d3406c
LukhanyoUlwembu <lomalu@ulwembubs.com>
2025-11-18 10:35:05 +0200

    Update MSAL redirect for production

--- src/authConfig.js
+++ src/authConfig.js
@@ -5,7 +5,9 @@ export const msalConfig = {
   auth: {
     clientId: "543e6776-91c0-48eb-bf30-b0a1bd2f0592", 
     authority: "https://login.microsoftonline.com/263f6167-9311-4dd0-88be-0a6ec8bed433", 
-    redirectUri: "http://localhost:5173", 
+    // redirectUri: "http://localhost:5173", 
+    redirectUri: "https://ulwemburecruite.netlify.app",
+
   },
   cache: {
     cacheLocation: "localStorage", 
